e150211acd11f2d612dde5fed12e368df917e0be
Shamim742 <shamimislam2742@gmail.com>
2023-01-29 12:18:57 +0600

    array problem solve

---
+++ prac-day-1/array1.js
@@ -0,0 +1,7 @@
+// target: display full array
+let myLearnings = ['variable', 'let', 'var', 'const', 'array', 'conditional', 'loops', 'index'];
+
+for (let i = 0; i < myLearnings.length; i++) {
+    let learnings = myLearnings[i];
+    console.log(learnings);
+}
