dc81c9623bb84a0ba03ee17b9e57f3107d176b34
Shamim742 <shamimislam2742@gmail.com>
2023-01-29 10:48:23 +0600

    practice problem 1 & 2

---
+++ loops/practice-problem.js
@@ -0,0 +1,14 @@
+let myWorks = ['Unlocking Module', 'watching tutorials and Practice', 'Notes', 'Practice', 'Support Session'];
+// Target : display array 10 times
+for (let i = 0;i <= 10; i++) {
+    let i = myWorks;
+    console.log(i);   
+}
+// target: display array reverse 10 times
+for (let i = 4;i >= 0; i--) {
+    let work = myWorks[i];
+    for (let i = 0; i <= 10; i++){
+        let i = work;
+        console.log(i);
+    }
+}
