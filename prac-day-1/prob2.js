b7a1c1ba2f373c11c4ab643627266f9939824531
Shamim742 <shamimislam2742@gmail.com>
2023-01-29 12:11:19 +0600

    add some problem related to for loop

--- prac-day-1/prob2.js
+++ prac-day-1/prob2.js
@@ -4,3 +4,15 @@ for(let i = 1; i < 40; i++){
     let i = mood;
     console.log(i);
 }
+// target: display a 58 to 98
+for (let i = 58; i < 99; i++) {
+    console.log(i)
+}
+// target: display 412 to 456 every even number
+for (let i = 412; i <= 456; i +=2) {
+    console.log(i)
+}
+// target: display 581 to i 623 every odd number
+for (let i = 581; i <= 623; i += 2) {
+    console.log(i)
+}
