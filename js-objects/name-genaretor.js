3a5a19de6d1f6678f0b612ea6f6a1c5147740673
Shamim742 <shamimislam2742@gmail.com>
2023-01-30 19:09:11 +0600

    problem-3

---
+++ js-objects/name-genaretor.js
@@ -0,0 +1,10 @@
+// write a function that has 2 parameters firstName and lastName and that return fullName
+function fullName (firstName, lastName) {
+    let fullName = firstName + " " + lastName;
+    return fullName;
+}
+let firstName1 = 'Md. Shamim';
+let lastName1 = 'Islam';
+
+let fullName1 = fullName(firstName1, lastName1);
+console.log(fullName1);
