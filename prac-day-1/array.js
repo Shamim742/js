b8f0ff8ba38b4d81bd2555cf4436378ddd34ae03
Shamim742 <shamimislam2742@gmail.com>
2023-01-30 18:27:13 +0600

    wh loop

--- prac-day-1/array.js
+++ prac-day-1/array.js
@@ -1,3 +1,4 @@
+// target: pass every value over 200
 let booksPrice = [100, 80, 300, 250, 180, 220, 170, 190, 120, 180, 140, 150];
 
 for (let i = 0; i <= booksPrice.length; i++) {
---
+++ prac-day-1/wh-loop.js
@@ -0,0 +1,6 @@
+let myPhones = ['Symphony', 'Samsung', 'Vivo'];
+let i = myPhones;
+while (i <= 3) {
+    i++;
+    console.log(i);
+}
