9053f743c8fc398ebd9a654dab4044640885546e
Shamim742 <shamimislam2742@gmail.com>
2023-01-30 18:55:47 +0600

    problem-2

---
+++ js-objects/upper.js
@@ -0,0 +1,14 @@
+// write a function that parameter returns to lowercase
+function toLower(a) {
+    let b = a;
+    console.log(b.toLowerCase());
+};
+toLower('SHAMIM');
+
+// write a function that returns any input to square
+
+function square(a) {
+    let c = a**2;
+    console.log(c);
+}
+square(6);
